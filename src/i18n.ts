f8ff41b1b4a88b4251eaf93ac5ab9400537b54b8
Idan Asayag <idanasayag0@gmail.com>
2024-07-24 18:51:00 +0300

    add more language

--- src/i18n.ts
+++ src/i18n.ts
@@ -11,6 +11,8 @@ i18n.use(initReactI18next).init({
 (Object.keys(translations) as (keyof typeof translations)[]).forEach((key) => {
   i18n.addResourceBundle('en', 'translation', { [key]: translations[key][0] });
   i18n.addResourceBundle('he', 'translation', { [key]: translations[key][1] });
+  i18n.addResourceBundle('fr', 'translation', { [key]: translations[key][2] });
+  i18n.addResourceBundle('zh', 'translation', { [key]: translations[key][3] });
 });
 
 export default i18n;
