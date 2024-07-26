3a3a90a06f055b55a46ec16760ce34a71646f70a
Idan Asayag <idanasayag0@gmail.com>
2024-07-26 18:45:41 +0300

    done before validation

--- src/pages/ResetPassword/ResetPasswordPage.tsx
+++ src/pages/ResetPassword/ResetPasswordPage.tsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axiosRequest from '../../utils/restApi';
-import { useNavigate } from 'react-router';
-import { useUser } from '../../store/store';
+import { Navigate, useNavigate } from 'react-router';
+import { useIsAuth } from '../../store/store';
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Grid,
+  useTheme,
+} from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { toastify } from '../../utils/utils';
 
 const ResetPassword: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -10,21 +22,36 @@ const ResetPassword: React.FC = () => {
   const [newPassword, setNewPassword] = useState('');
   const navigate = useNavigate();
   const [otp, setOtp] = useState('');
-  const currentUser = useUser((state) => state.user);
+  const { isAuthenticate } = useIsAuth();
+  const theme = useTheme();
+  const { t } = useTranslation();
 
-  useEffect(() => {
-    if (currentUser) {
-      navigate('/');
-    }
-  }, [navigate, currentUser]);
+  if (isAuthenticate) {
+    return <Navigate to='/' />;
+  }
 
   const handleSendOtp = async () => {
+    if (!email) {
+      toastify({
+        type: 'error',
+        message: 'Please enter your email',
+        position: 'top-right',
+      });
+      return;
+    }
     try {
       await axiosRequest({
         data: { email },
         method: 'POST',
         url: '/auth/send-otp',
-      }).then(() => setIsEmailSent(true));
+      }).then(() => {
+        toastify({
+          type: 'success',
+          message: 'Email sent successfully',
+          position: 'top-right',
+        });
+        setIsEmailSent(true);
+      });
     } catch (error) {
       console.error('Error sending OTP', error);
     }
@@ -36,7 +63,22 @@ const ResetPassword: React.FC = () => {
         data: { email, otp },
         method: 'POST',
         url: '/auth/verify-otp',
-      }).then(() => setIsOtpVerified(true));
+      }).then((res) => {
+        if (res.status === 400) {
+          toastify({
+            type: 'error',
+            message: 'Invalid OTP',
+            position: 'top-right',
+          });
+          return;
+        }
+        toastify({
+          type: 'success',
+          message: 'OTP verified successfully',
+          position: 'top-right',
+        });
+        setIsOtpVerified(true);
+      });
     } catch (error) {
       console.error('Error verifying OTP', error);
     }
@@ -55,46 +97,92 @@ const ResetPassword: React.FC = () => {
   };
 
   return (
-    <div>
-      {!isEmailSent && (
-        <div>
-          <h2>Reset Password</h2>
-          <input
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder='Enter your email'
-          />
-          <button onClick={handleSendOtp}>Send OTP</button>
-        </div>
-      )}
+    <Container maxWidth='sm'>
+      <Box mt={5}>
+        <Typography variant='h4' gutterBottom>
+          {t('Reset Password')}
+        </Typography>
+        {!isEmailSent && (
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                type='email'
+                label={t('Email')}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                variant='contained'
+                color='primary'
+                onClick={handleSendOtp}
+              >
+                {t('Send OTP')}
+              </Button>
+            </Grid>
+          </Grid>
+        )}
 
-      {isEmailSent && !isOtpVerified && (
-        <div>
-          <h2>Verify OTP</h2>
-          <input
-            type='text'
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-            placeholder='Enter OTP'
-          />
-          <button onClick={handleVerifyOtp}>Verify OTP</button>
-        </div>
-      )}
+        {isEmailSent && !isOtpVerified && (
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                type='text'
+                label={t('OTP')}
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                variant='contained'
+                color='primary'
+                onClick={handleVerifyOtp}
+              >
+                {t('Verify OTP')}
+              </Button>
+            </Grid>
+          </Grid>
+        )}
 
-      {isOtpVerified && (
-        <div>
-          <h2>Change Password</h2>
-          <input
-            type='password'
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-            placeholder='Enter new password'
-          />
-          <button onClick={handleChangePassword}>Change Password</button>
-        </div>
-      )}
-    </div>
+        {isOtpVerified && (
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                type='password'
+                label={t('Password')}
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                variant='outlined'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                variant='contained'
+                color='primary'
+                onClick={handleChangePassword}
+              >
+                {t('Change Password')}
+              </Button>
+            </Grid>
+          </Grid>
+        )}
+        <Box marginTop={'0.5rem'}>
+          <NavLink style={{ color: theme.palette.secondary.main }} to='/signin'>
+            {t('Already have an account? Sign In')}
+          </NavLink>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
