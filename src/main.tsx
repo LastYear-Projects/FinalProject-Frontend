6d60bb8c3a66b02205f6d7007e7b147e5c7c5aa7
Idan Asayag <idanasayag0@gmail.com>
2024-08-23 10:33:39 +0300

    auto

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./styles.css";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles.css';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <I18nextProvider i18n={i18n}>
     <App />
   </I18nextProvider>
