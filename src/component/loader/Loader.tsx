72f91df1951d6c232f7e0e3d92e15965da4f2cba
DanDavidovich <davidovich.dan@gmail.com>
2024-08-25 13:12:44 +0300

    spinner in the middle of the screen

--- src/component/loader/Loader.tsx
+++ src/component/loader/Loader.tsx
@@ -1,9 +1,18 @@
-import { useTheme, CircularProgress } from '@mui/material';
+import {useTheme, CircularProgress, Box} from '@mui/material';
 
 const Loader = () => {
   const theme = useTheme();
   return (
-    <CircularProgress sx={{ color: theme.palette.secondary.contrastText }} />
+      <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+          }}
+      >
+        <CircularProgress sx={{ color: theme.palette.secondary.contrastText }} />
+      </Box>
   );
 };
 
